ef71ab1246e42a0a3be06645eb293a4f4f32b545
Mindaugas Greibus <mondhs@gmail.com>
2014-05-17 19:12:30 +0300

    added sample of import from local dir

--- impl/demo-py/robotas_ps.py
+++ impl/demo-py/robotas_ps.py
@@ -3,22 +3,25 @@ Created on Dec 29, 2013
 
 @author: Mindaugas Greibus
 '''
+import sys, os
+
+#sys.path.insert(0, '/home/mondhs/tmp/pocketsphinx-bin/usr/local/lib/python2.7/dist-packages/sphinxbase')
+#sys.path.insert(0, '/home/mondhs/tmp/pocketsphinx-bin/usr/local/lib/python2.7/dist-packages/pocketsphinx')
+
 
 from pocketsphinx import Decoder
-from os import environ, path
-#from sphinxbase import *
 
 MODELDIR = "../models"
 #MODELDIR = "/home/as/src/speech/sphinx/lt-pocketsphinx-tutorial/impl/models"
 
 # Create a decoder with certain model
 config = Decoder.default_config()
-config.set_string('-hmm', path.join(MODELDIR, 'hmm/lt.cd_cont_200/'))
-config.set_string('-jsgf', path.join(MODELDIR, 'lm/robotas.gram'))
-config.set_string('-dict', path.join(MODELDIR, 'dict/robotas.dict'))
+config.set_string('-hmm', os.path.join(MODELDIR, 'hmm/lt.cd_cont_200/'))
+config.set_string('-jsgf', os.path.join(MODELDIR, 'lm/robotas.gram'))
+config.set_string('-dict', os.path.join(MODELDIR, 'dict/robotas.dict'))
 decoder = Decoder(config)
 
-decoder.decode_raw(open(path.join(MODELDIR, '../test/audio/varyk_pirmyn-16k.wav'), 'rb'))
+decoder.decode_raw(open(os.path.join(MODELDIR, '../test/audio/varyk_pirmyn-16k.wav'), 'rb'))
 
 # Retrieve hypothesis.
 hypothesis = decoder.hyp()
